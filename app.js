462569f2a7f3abb5ee22c74b230109cda3f0c8ad
VillalbaLeandro <107075681+VillalbaLeandro@users.noreply.github.com>
2022-07-12 21:44:36 -0300

    act a heroku

--- app.js
+++ app.js
@@ -6,21 +6,21 @@ const app = express();
 app.use(express.static(path.resolve(__dirname, "./public")));
 app.use(express.urlencoded()); //interpretar desde el navegador los archivos json
 
-app.get("/", (req, res) =>{
-  res.sendFile(path.join(__dirname, "/views/home.html"))  
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 
-app.get("/register", (req, res) =>{
-  res.sendFile(path.join(__dirname, "/views/register.html"))  
+app.get("/register", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/register.html"))
 });
 
-app.get("/login", (req, res) =>{
-  res.sendFile(path.join(__dirname, "/views/login.html"))  
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/login.html"))
 });
 
 
 
 
-app.listen(3040, () =>{
-    console.log("se ha iniciado el servidor con EXPRESS http://localhost:3040");
+app.listen(3040, () => {
+  console.log("se ha iniciado el servidor con EXPRESS http://localhost:3040");
 })
